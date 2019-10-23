2ea65c944ea8b497ec68a148e8f0f019425129c3
anjitzero <45827003+anjitzero@users.noreply.github.com>
2019-10-23 12:43:02 +0545

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,20 @@
+$('.tags-carosel').owlCarousel({
+      autoplay:false,
+      autoplaySpeed:7000,
+      slideSpeed:3000,
+      margin:20,
+      nav:false,
+      loop:false,
+      items:5,
+      dots:false,
+      // mouseDrag:false,
+      responsiveClass:true,
+      responsive:{
+        0:{
+
+        },
+        768:{
+          // mouseDrag:true,
+        }
+      }
+    });
